73e492f051349a3f2e719d50d7c8c166ba4db747
Yamamuen <you@example.com>
2023-08-30 12:54:26 -0300

    cryptonews.py now fetches all available news

--- src/cryptonews.py
+++ src/cryptonews.py
@@ -1,4 +1,6 @@
-import os, json, time
+import os
+import json
+import time
 from datetime import datetime
 from datetime import date
 from datetime import timedelta
@@ -10,95 +12,81 @@ from urllib3.util.retry import Retry
 import pandas as pd
 import utils as u
 
-session = requests.Session()
-retry = Retry(connect=3, backoff_factor=0.5)
-adapter = HTTPAdapter(max_retries=retry)
-session.mount('http://', adapter)
-session.mount('https://', adapter)
-
 
 load_dotenv('../config_files/.env')
 
 
 def get_cryptonews(ticker, sd, ed):
-    """Retrieves all news related to the ticker in CSV format.
-    You must have an environ variable containing the token.
+    """Fetches News data from Crypto News API between the chosen dates. An API TOKEN is required!
 
     Args:
-        ticker (str): crypto ticker
+        ticker (string): Crypto Coin ticker
+        sd (datetime): Starting Date
+        ed (datetime): End Date
+
+    Returns:
+        _type_: _description_
     """
 
-    from_date = sd
+    # an .env with the crypto news token is required
     TOKEN = os.getenv('CRYPTONEWS_API_TOKEN')
-    days =  ed - sd
+
+    # Session config (necessary in case the first request attempt fails)
     session = requests.Session()
     retry = Retry(connect=10, backoff_factor=0.5)
     adapter = HTTPAdapter(max_retries=retry)
     session.mount('http://', adapter)
     session.mount('https://', adapter)
 
-    while True:
+    curr_date = sd  # current date
+    end_date = sd  # end date
+    while True:  # date loop
 
-        data = sd.strftime('%m%d%Y')
-        
-        url = \
-            f"""
-            https://cryptonews-api.com/api/v1?tickers={ticker}&sortby=rank&extra-fields=id,eventid,rankscore&items=100&page=1&date={data}-{data}&token={TOKEN}
-            """
-        
-        api_return = session.get(url).text
-        pydict = json.loads(api_return)
-        # Request Error Handling
-        # if the request is not sucessful, 3 extra attempts will be made with pauses
-        # tries = 5
-        # while tries>=0:
-        #     try:
-        #         pydict = json.loads(api_return)
-        #         break
-        #     except requests.exceptions.ConnectionError:
-        #         r.status_code = "Connection refused"
-        #         if tries == 0:
-        #             # Quits and saves data as json file
-        #             with open('cryptonews.json', 'w', encoding='utf-8') as f:
-        #                 json.dump(x, f, ensure_ascii=False, indent=4)
-        #             raise
-        #         else:
-        #             time.sleep(4)
-        #             tries -= 1
-        #             continue
-            
+        data = curr_date.strftime('%m%d%Y')
+        pg = 1
+        while True:  # repeats until last page is reached
+            print(f"Fetching page {pg} for {data}")
+            url = \
+                f"""
+                https://cryptonews-api.com/api/v1?tickers={ticker}&sortby=rank&extra-fields=id,eventid,rankscore&items=100&page={pg}&date={data}-{data}&token={TOKEN}
+                """
 
-        try:
-            # print(f"Loading 100 top news at {dt}...")
-            pass
-        except:
+            api_return = session.get(url).text
+            pydict = json.loads(api_return)
+
+            # there is a message when the URL returned an error and data otherwise.
+            if 'message' not in pydict:
+                try:
+                    # if x was already assembled
+                    dict_data['data'].extend(pydict['data'])
+                except:
+                    dict_data = pydict  # creating the data cumulative dict
+
+                pg += 1  # moving on to the next page
+
+            # If there is an error we assume the amount of news in that day ended (= reached last page)
+            else:
+                # moving on to the next day!
+                curr_date = curr_date + timedelta(days=1)
+                break
+
+        if curr_date > end_date:
             break
-        
-        if 'message' not in pydict and ed>=sd:        
-            try:
-                x['data'].extend(pydict['data'])
-            except:
-                x = pydict
-            
-            sd = sd + timedelta(days=1)
 
-        else:
+    # Creating JSON file:
+    with open(f"../data/raw_cryptonews_{ticker}_from_{sd.strftime('%Y%m%d')}_to_{ed.strftime('%Y%m%d')}.json", 'w', encoding='utf-8') as f:
+        json.dump(dict_data, f, ensure_ascii=False, indent=4)
 
-            with open(f"../data/raw_cryptonews_{ticker}_from_{from_date.strftime('%Y%m%d')}_to_{ed.strftime('%Y%m%d')}.json", 'w', encoding='utf-8') as f:
-                json.dump(x, f, ensure_ascii=False, indent=4)
+    # Creating CSV file with UTC date adjustments:
 
-            with open(f"../data/raw_cryptonews_{ticker}_from_{from_date.strftime('%Y%m%d')}_to_{ed.strftime('%Y%m%d')}.json", 'r') as f:
-                dict_data = json.load(f)
-            df_raw_crypto_news = pd.DataFrame(dict_data ['data'])
-            
-            df_formatted_crypto_news = u.create_utc_date_column(df_raw_crypto_news)
+    df_raw_crypto_news = pd.DataFrame(dict_data['data'])
 
-            df_formatted_crypto_news.to_csv(f"../data/formatted_cryptonews_{ticker}_from_{from_date.strftime('%Y%m%d')}_to_{ed.strftime('%Y%m%d')}.csv", index=False)
+    df_formatted_crypto_news = u.create_utc_date_column(
+        df_raw_crypto_news)  # from utils.py
 
-            break
+    df_formatted_crypto_news.to_csv(
+        f"../data/formatted_cryptonews_{ticker}_from_{sd.strftime('%Y%m%d')}_to_{ed.strftime('%Y%m%d')}.csv", index=False)
+
 
     return print("Done")
 
-
-# if __name__ == '__main__':
-#     get_cryptonews('BTC', date(2021,1,1))
